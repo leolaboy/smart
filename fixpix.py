Fixed the noise calculation. Fixed fixpix.

fixpix.py
@@ -161,15 +161,15 @@ def fixpix_rs(im, iternum=3, quiet=True):
                 hoti    = np.where(box == hotval)[0][0]
 
                 # coords in original image
-                hotx    = hoti % 5 + i-2 
-                hoty    = hoti / 5 + j-2
+                hotx    = int(hoti % 5 + i-2) 
+                hoty    = int(hoti / 5 + j-2)
 
                 coldval = np.min(box)
                 coldi   = np.where(box == coldval)[0][0]
 
                 # coords in original image
-                coldx   = coldi % 5 + i-2
-                coldy   = coldi / 5 + j-2
+                coldx   = int(coldi % 5 + i-2)
+                coldy   = int(coldi / 5 + j-2)
 
                                     # begin the decision process for the hottest pixel
   

image_lib.py
@@ -198,6 +198,21 @@ def extract_spectra(obj, flat, noise, obj_range, sky_range_top, sky_range_bot, e
     """
     #print('OBJ RANGE:', obj_range)
     #sys.exit()
+
+    ### TESTING AREA XXX
+    """
+    print(obj_range)
+    print(sky_range_top, sky_range_bot)
+    import matplotlib.pyplot as plt
+    plt.figure(20)
+    plt.imshow(obj)
+    plt.figure(21)
+    plt.imshow(flat)
+    plt.figure(22)
+    plt.imshow(noise)
+    #plt.show()
+    #sys.exit()
+    """
     
     obj_sum     = np.sum(obj[i, :] for i in obj_range)
     flat_sum    = np.sum(flat[i, :] for i in obj_range)
@@ -206,6 +221,8 @@ def extract_spectra(obj, flat, noise, obj_range, sky_range_top, sky_range_bot, e
 
     sky_top_sum = np.sum(obj[i, :] for i in sky_range_top)
     sky_bot_sum = np.sum(obj[i, :] for i in sky_range_bot)
+    #print(sky_top_sum)
+    #print(sky_bot_sum)
     
     if len(sky_range_top) > 0:
         top_bg_mean = (sky_top_sum / len(sky_range_top)).mean()
@@ -217,6 +234,12 @@ def extract_spectra(obj, flat, noise, obj_range, sky_range_top, sky_range_bot, e
         bot_bg_mean = None
     
     sky_mean = (sky_top_sum + sky_bot_sum) / (len(sky_range_top) + len(sky_range_bot))
+    """
+    print(top_bg_mean)
+    print(bot_bg_mean)
+    print(sky_mean)
+    """
+
 
 #     sky_mean -= np.median(sky_mean) 
 
@@ -228,8 +251,25 @@ def extract_spectra(obj, flat, noise, obj_range, sky_range_top, sky_range_bot, e
     sky_noise_top_sum = np.sum(noise[i, :] for i in sky_range_top)
     sky_noise_bot_sum = np.sum(noise[i, :] for i in sky_range_bot)
     
-    k = np.square(len(obj_range)) / np.square((len(sky_range_top) + len(sky_range_bot)))
+    k = float(np.square(len(obj_range))) / float(np.square((len(sky_range_top) + len(sky_range_bot))))
+    """
+    print(k)
+    print(np.square(len(obj_range)) / np.square((len(sky_range_top) + len(sky_range_bot))))
+    print(np.square(len(obj_range)))
+    print(np.square((len(sky_range_top) + len(sky_range_bot))))
+    print()
+    """
     noise_sp = np.sqrt(obj_noise_sum + (k * (sky_noise_top_sum + sky_noise_bot_sum)))
+    """
+    print(obj_noise_sum)
+    print((k * (sky_noise_top_sum + sky_noise_bot_sum)))
+    print(sky_noise_top_sum)
+    print(sky_noise_bot_sum)
+    print(noise_sp)
+
+    plt.show()
+    sys.exit()
+    """
     
     if eta is not None:
         #etalon_sum  = np.sum(eta[i, :] for i in obj_range) 

nirspec_lib.py
@@ -15,18 +15,22 @@ def calc_noise_img(obj, flat, integration_time):
     flat is expected to be normalized and both obj and flat are expected to be rectified
     """
     
-    G  = 5.8     
-    RN = 23.0
-    DC = 0.8
+    G  = 5.8  # e-/ADU    
+    RN = 23.0 # e-
+    DC = 0.8  # e-/second/pixel
     
     # calculate photon noise
-    noise = obj / G
+    #noise = obj / G # What is this?
+    noise = obj # This is ALREADY in ADU!
+    #noise = obj * G # This is in electrons
     
     # add read noise
-    noise += np.square(RN / G)
+    noise += np.square(RN / G) # This is in ADU
+    #noise += np.square(RN) # This is in electrons
     
     # add dark current noise
-    noise += (DC / G) * integration_time
+    noise += (DC / G) * integration_time # This is in ADU
+    #noise += (DC / G) * integration_time # This is in electrons
     
     # divide by normalized flat squared
     noise /= np.square(flat)